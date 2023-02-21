f2214138ac9722383063521e98c93eaafce61cac
Khang <khang.lenguyen192@gmail.com>
2023-02-21 17:07:13 +0700

    update day off page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import "./index.css";
 
-import { Breadcrumb, Layout, Menu } from "antd";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./views/pages/home/HomePage";
 import LoginPage from "./views/pages/login/LoginPage";
-import LeftSideBar from "./views/components/LeftSideBar";
-import TopBar from "./views/components/TopBar";
-import MainLayout from "./views/main-layout/MainLayout";
 import DayOffPage from "./views/pages/dayoff/DayOffPage";
+import MainLayout from "./views/main-layout/MainLayout";
 
 const App = () => {
-  if (true) {
+  var isLoggedIn = true;
+
+  if (isLoggedIn) {
     return (
       <Router>
         <Routes>
@@ -19,7 +18,14 @@ const App = () => {
             <Route path="home" element={<HomePage />} />
             <Route path="day-off" element={<DayOffPage />} />
           </Route>
-          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/*" element={<LoginPage />} />
         </Routes>
       </Router>
     );
