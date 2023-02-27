358cadb6a07e65258ba4453950f93a09800c503c
Tiến Lộc <tienlocak14@gmail.com>
2023-02-28 01:50:11 +0700

    Create department page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./views/pages/home/HomePage";
 import LoginPage from "./views/pages/login/LoginPage";
 import DayOffPage from "./views/pages/dayoff/DayOffPage";
 import MainLayout from "./views/main-layout/MainLayout";
+import DepartmentPage from "./views/pages/department/DepartmentPage";
 
 const App = () => {
   var isLoggedIn = true;
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route path="home" element={<HomePage />} />
+            <Route path="department" element={<DepartmentPage />} />
             <Route path="day-off" element={<DayOffPage />} />
           </Route>
         </Routes>
--- src/views/pages/home/HomePage.js
+++ src/views/pages/home/HomePage.js
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import "./HomePage.css";
 
 export default function HomePage() {
