a5e86a747f5197f0dd6e64da9161954cfff469eb
Khang <khang.lenguyen192@gmail.com>
2023-05-13 13:43:35 +0700

    refactor code

--- src/views/pages/dayoff/DayOffPage.js
+++ src/views/pages/dayoff/DayOffPage.js
@@ -9,9 +9,12 @@ import COLORS from "../../../common/constants/Colors";
 import Popup from "../../components/Popup";
 import TextArea from "rc-textarea";
 import { createEventId } from "../../../common/utils/Utils";
-import './DayOff.css';
+import "./DayOff.css";
 import { Checkbox, Input, Table, Tag } from "antd";
-import { typeDayOff, specialDayType } from "../../../common/constants/Constants";
+import {
+  typeDayOff,
+  specialDayType,
+} from "../../../common/constants/Constants";
 import DayOffService from "../../../services/DayOffService";
 import { useSelector } from "react-redux";
 import { event } from "jquery";
@@ -37,14 +40,12 @@ export default function DayOffPage() {
         // console.log("data: ", data);
         if (data.payload != null) {
           const init = data.payload.map((item) => ({
-            title: typeDayOff.find(x => x.option === item.option).type,
+            title: typeDayOff.find((x) => x.option === item.option).type,
             start: item.dateTime,
             id: item.id,
           }));
           setInitEvents(init);
-        }
-        else
-          setInitEvents(null);
+        } else setInitEvents(null);
       });
     }
     fetchData();
@@ -52,30 +53,28 @@ export default function DayOffPage() {
 
   function onViewListDayOff() {
     setShowList(!isShowList);
-    DayOffService.getDayOff(user.userId, user.token).then(
-      (res) => {
-        const response = res.data;
+    DayOffService.getDayOff(user.userId, user.token).then((res) => {
+      const response = res.data;
 
-        if (response.payload != null) {
-          // console.log("call api: ", response.payload);
-          let i = 1;
-          var dayoff = response.payload.map((item) => {
-            return {
-              id: i++,
-              reason: item.reason,
-              date: new Date(item.dateTime).toLocaleDateString("vi-VN"),
-              type: typeDayOff.find(x => x.option == item.option).type,
-              status: {
-                key: 1,
-                value: "Đã chấp nhận",
-              },
-            };
-          });
+      if (response.payload != null) {
+        // console.log("call api: ", response.payload);
+        let i = 1;
+        var dayoff = response.payload.map((item) => {
+          return {
+            id: i++,
+            reason: item.reason,
+            date: new Date(item.dateTime).toLocaleDateString("vi-VN"),
+            type: typeDayOff.find((x) => x.option == item.option).type,
+            status: {
+              key: 1,
+              value: "Đã chấp nhận",
+            },
+          };
+        });
 
-          setlistDayOff(dayoff);
-        }
+        setlistDayOff(dayoff);
       }
-    );;
+    });
   }
 
   function onCancelPopup() {
@@ -84,17 +83,15 @@ export default function DayOffPage() {
 
   function onConfirmDayOff() {
     setShowPopupConfirm(false);
-    info.forEach(e => {
-      dayoff.push(
-        {
-          userId: user.userId,
-          dateTime: e._instance.range.start.toISOString(),
-          option: typeDayOff.find(x => x.type === e._def.title).option,
-          type: specialDayType.find(x => x.type === "DayOff").value,
-          reason: reason,
-          isUrgent: urgent,
-        }
-      );
+    info.forEach((e) => {
+      dayoff.push({
+        userId: user.userId,
+        dateTime: e._instance.range.start.toISOString(),
+        option: typeDayOff.find((x) => x.type === e._def.title).option,
+        type: specialDayType.find((x) => x.type === "DayOff").value,
+        reason: reason,
+        isUrgent: urgent,
+      });
     });
 
     DayOffService.createDayOff(dayoff, user.token).then((res) => {
@@ -125,7 +122,10 @@ export default function DayOffPage() {
     customButtons: {
       submitDayOff: {
         text: "Xin nghỉ phép",
-        click: () => { console.log("info: ", info); setShowPopupConfirm(!showPopupConfirm); },
+        click: () => {
+          console.log("info: ", info);
+          setShowPopupConfirm(!showPopupConfirm);
+        },
       },
       viewListLeaving: {
         text: "Danh sách",
@@ -144,19 +144,19 @@ export default function DayOffPage() {
       title: "Lý do",
       dataIndex: "reason",
       key: "reason",
-      render: (text) => <a>{ text }</a>,
+      render: (text) => <a>{text}</a>,
     },
     {
       title: "Ngày xin nghỉ",
       dataIndex: "date",
       key: "date",
-      render: (text) => <a>{ text }</a>,
+      render: (text) => <a>{text}</a>,
     },
     {
       title: "Hình thức",
       dataIndex: "type",
       key: "type",
-      render: (text) => <a>{ text }</a>,
+      render: (text) => <a>{text}</a>,
     },
     {
       title: "Trạng thái",
@@ -176,8 +176,8 @@ export default function DayOffPage() {
             break;
         }
         return (
-          <Tag color={ color } key={ status.key }>
-            { status.value }
+          <Tag color={color} key={status.key}>
+            {status.value}
           </Tag>
         );
       },
@@ -192,7 +192,7 @@ export default function DayOffPage() {
             title="Xóa"
             class="remove-dayoff-bt btn btn-icon btn-sm waves-effect waves-light btn-danger"
             type="button"
-            onClick={ (id) => deleteEvent(id) }
+            onClick={(id) => deleteEvent(id)}
           >
             <i class="mdi mdi-delete-circle"></i>
           </button>
@@ -212,7 +212,10 @@ export default function DayOffPage() {
 
       while (currentDate < endDate) {
         if (initEvents != null) {
-          let findInit = initEvents.find(x => new Date(x.start).getTime() === new Date(currentDate).getTime());
+          let findInit = initEvents.find(
+            (x) =>
+              new Date(x.start).getTime() === new Date(currentDate).getTime()
+          );
           if (findInit != null) {
             currentDate = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
             continue;
@@ -220,32 +223,38 @@ export default function DayOffPage() {
         }
 
         const events = calendarApi.getEvents();
-        let opt = events.find(x => new Date(x.start).getTime() === new Date(currentDate).getTime());
-        let ind = result.findIndex(x => new Date(x.start).getTime() === new Date(currentDate).getTime());
+        let opt = events.find(
+          (x) => new Date(x.start).getTime() === new Date(currentDate).getTime()
+        );
+        let ind = result.findIndex(
+          (x) => new Date(x.start).getTime() === new Date(currentDate).getTime()
+        );
 
         if (ind !== -1) {
-          if (opt.title === 'AM-OFF') {
-            opt.setProp('title', 'PM-OFF');
+          if (opt.title === "AM-OFF") {
+            opt.setProp("title", "PM-OFF");
             result[ind] = opt;
-          }
-          else if (opt.title === 'PM-OFF') {
-            opt.setProp('title', 'DAY-OFF');
+          } else if (opt.title === "PM-OFF") {
+            opt.setProp("title", "DAY-OFF");
             result[ind] = opt;
-          }
-          else {
+          } else {
             opt.remove();
             result.splice(ind, 1);
             ind = -1;
           }
           // console.log("opt: ", opt);
-        }
-        else {
+        } else {
           calendarApi.addEvent({
             start: currentDate,
             title: "AM-OFF",
           });
 
-          let e = calendarApi.getEvents().find(x => new Date(x.start).getTime() === new Date(currentDate).getTime());
+          let e = calendarApi
+            .getEvents()
+            .find(
+              (x) =>
+                new Date(x.start).getTime() === new Date(currentDate).getTime()
+            );
           result.push(e);
         }
         currentDate = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
@@ -257,25 +266,31 @@ export default function DayOffPage() {
   };
 
   const renderEventContent = (eventInfo) => {
-    let color = typeDayOff.find(e => e.type === eventInfo.event.title);
+    let color = typeDayOff.find((e) => e.type === eventInfo.event.title);
     return (
-      <div style={ {
-        display: "flex",
-        width: "-webkit-fill-available",
-        borderRadius: "2px",
-        padding: "8px 10px",
-        margin: "0 5px",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: color.bgColor
-      } }>
-        <i className="text-light text-center">{ eventInfo.event.title }</i>
+      <div
+        style={{
+          display: "flex",
+          width: "-webkit-fill-available",
+          borderRadius: "2px",
+          padding: "8px 10px",
+          margin: "0 5px",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: color.bgColor,
+        }}
+      >
+        <i className="text-light text-center">{eventInfo.event.title}</i>
       </div>
     );
   };
 
   const handleEventClick = (clickInfo) => {
-    if (!alert(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    if (
+      !alert(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
       clickInfo.event.remove();
     }
   };
@@ -287,16 +302,16 @@ export default function DayOffPage() {
   return (
     <div class="row">
       <ConfirmDialog
-        isShow={ showPopupConfirm }
+        isShow={showPopupConfirm}
         title="Xin nghỉ phép"
-        onCancel={ onCancelPopup }
+        onCancel={onCancelPopup}
         mainButtonText="Xác nhận"
         subButtonText="Đóng"
-        mainButtonClick={ onConfirmDayOff }
-        subButtonClick={ onCancelPopup }
+        mainButtonClick={onConfirmDayOff}
+        subButtonClick={onCancelPopup}
       >
         <form onSubmit="submitWorkRemoteDate()" autocomplete="off">
-          <div class="modal-body" style={ { padding: "0 0 10px 0" } }>
+          <div class="modal-body" style={{ padding: "0 0 10px 0" }}>
             <div class="row">
               <label class="col-sm-12 col-form-label">Lý do</label>
               <div class="col-sm-12">
@@ -305,11 +320,13 @@ export default function DayOffPage() {
                     class="form-group"
                     size="large"
                     required="true"
-                    onChange={ (e) => { setReason(e.target.value); } }
+                    onChange={(e) => {
+                      setReason(e.target.value);
+                    }}
                   ></Input>
                 </div>
                 <div className="pt-3 float-right">
-                  <Checkbox onChange={ onChange }>Xin nghỉ khẩn cấp</Checkbox>
+                  <Checkbox onChange={onChange}>Xin nghỉ khẩn cấp</Checkbox>
                 </div>
               </div>
             </div>
@@ -320,12 +337,12 @@ export default function DayOffPage() {
       <div class="col-12 grid-margin">
         <div class="card">
           <div class="card-body">
-            { isShowList ? (
+            {isShowList ? (
               <div>
                 <button
                   type="button"
                   class="fc-viewListLeaving-button fc-button fc-button-primary float-right"
-                  onClick={ onViewListDayOff }
+                  onClick={onViewListDayOff}
                 >
                   <i class="mdi mdi-calendar-today"></i>
                 </button>
@@ -337,145 +354,29 @@ export default function DayOffPage() {
                 </span>
 
                 <div class="table-data">
-                  <Table
-                    columns={ columns }
-                    dataSource={ listDayOff }
-                  ></Table>
+                  <Table columns={columns} dataSource={listDayOff}></Table>
                 </div>
               </div>
             ) : (
               <FullCalendar
-                plugins={ options.plugins }
-                headerToolbar={ options.header }
-                footerToolbar={ options.footer }
-                eventLimitText={ options.eventLimitText }
-                editable={ options.editable }
-                selectable={ true }
-                selectMirror={ true }
-                dayMaxEvents={ true }
-                weekends={ true }
-                events={ initEvents }
-                select={ selectHandle }
-                customButtons={ options.customButtons }
-                buttonText={ options.buttonText }
-                eventContent={ renderEventContent } // custom render function
-                eventClick={ handleEventClick }
-                height={ 800 }
-                locale={ viLocale }
+                plugins={options.plugins}
+                headerToolbar={options.header}
+                footerToolbar={options.footer}
+                eventLimitText={options.eventLimitText}
+                editable={options.editable}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                weekends={true}
+                events={initEvents}
+                select={selectHandle}
+                customButtons={options.customButtons}
+                buttonText={options.buttonText}
+                eventContent={renderEventContent} // custom render function
+                height={800}
+                locale={viLocale}
               />
-            ) }
-
-            {/* <Popup trigger={ buttonPopup } setTrigger={ setButtonPopup } onSubmit={ onSubmit } title="Xin nghỉ phép">
-              <div className='container-dayoff'>
-                <div className='content-dayoff'>
-                  <div className='reason'>
-                    <h5>Lý do</h5>
-                    <TextArea rows={ 6 } placeholder="Lý do xin nghỉ phép"
-                      style={ { backgroundColor: COLORS.graynish, maxHeight: "calc(100vh - 28rem)" } }
-                      onChange={ (e) => { setReason(e.target.value); } }
-                    />
-                    <div className="float-right">
-                      <Checkbox onChange={ onChange }>Xin nghỉ khẩn cấp</Checkbox>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Popup> */}
-
-            {/* <div>
-              <button
-                type="button"
-                class="fc-viewListLeaving-button fc-button fc-button-primary float-right"
-              >
-                <i class="mdi mdi-calendar-today"></i>
-              </button>
-              <h4 class="card-title mb-4">Danh sách xin nghỉ phép</h4>
-
-              <span class="d-flex align-items-center justify-content-end mr-0 mb-3">
-                Tìm: &nbsp;
-                <app-text-box type="text"></app-text-box>
-              </span>
-
-              <div class="table-data">
-                <table nzShowPagination nzShowSizeChanger nzPageSize="50">
-                  <thead nzSingleSort>
-                    <tr>
-                      <th nzShowSort nzSortKey="user.userName">
-                        Nhân viên
-                      </th>
-                      <th nzShowSort nzSortKey="reason">
-                        Lý do nghỉ
-                      </th>
-                      <th nzShowSort nzSortKey="dateTime">
-                        Ngày nghỉ
-                      </th>
-                      <th nzShowSort nzSortKey="option">
-                        Thời gian nghỉ
-                      </th>
-                      <th nzShowSort nzSortKey="dayOffStatus">
-                        Trạng thái
-                      </th>
-                      <th>Tác vụ</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    <tr>
-                      <td>
-                        <app-shared-image></app-shared-image>
-                        <label class="ml-2">{dayoff.user.userName}</label>
-                      </td>
-                      <td>{dayoff.reason}</td>
-                      <td>{dayoff.dateTime | date}</td>
-                      <td>
-                        <span class="badge badge-success font-13">Cả ngày</span>
-                        <span class="badge badge-danger font-13">
-                          Buổi sáng
-                        </span>
-                        <span class="badge badge-warning font-13">
-                          Buổi chiều
-                        </span>
-                      </td>
-                      <td>
-                        <span class="badge badge-success font-13">
-                          Đã chấp nhận
-                        </span>
-                        <span class="badge badge-danger font-13">
-                          Đã từ chối
-                        </span>
-                        <span class="badge badge-warning font-13">
-                          Chờ xác nhận
-                        </span>
-                      </td>
-                      <td>
-                        <button
-                          type="button"
-                          class="leving-detail-bt btn btn-icon btn-sm waves-effect waves-light btn-custom mr-2"
-                          title="Chấp nhận"
-                        >
-                          {" "}
-                          <i class="fa fa-check"></i>
-                        </button>
-                        <button
-                          class="leving-detail-bt btn btn-icon btn-sm waves-effect waves-light btn-danger"
-                          title="Từ chối"
-                        >
-                          {" "}
-                          <i class="fa fa-times"></i>
-                        </button>
-                        <button
-                          title="Xóa"
-                          class="remove-dayoff-bt btn btn-icon btn-sm waves-effect waves-light btn-danger"
-                          type="button"
-                        >
-                          <i class="mdi mdi-delete-circle"></i>
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div> */}
+            )}
           </div>
         </div>
       </div>
